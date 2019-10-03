00e35717825059f08a315d8839408a87922f0e5a
lerma1 <lerma1@mail.ru>
2019-10-03 18:04:32 +0700

    start navigation

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { Provider} from 'react-redux'
 import App from "./components/app";
 import {createStore} from "redux";
 import rootReducer from "./store/reducers";
-//import BrowserRouter from "react-router-dom/modules/BrowserRouter";
+import BrowserRouter from "react-router-dom/modules/BrowserRouter";
 
 
 
 const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 var mainElem = document.getElementById('root');
-render(<Provider store={store}> <App/> </Provider>, mainElem);
+render(<BrowserRouter><Provider store={store}> <App/> </Provider></BrowserRouter>, mainElem);
 
 
